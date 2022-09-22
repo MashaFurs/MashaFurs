2b491d19f6c382fa6650bb1a9c5b48427c0f3de1
MashaFurs <Mashafurs3371403@yandex.ru>
2022-09-22 19:29:25 +0300

    hw

--- FD1-194-22/Makets/Kamchatka/js/script.js
+++ FD1-194-22/Makets/Kamchatka/js/script.js
@@ -1,7 +1,21 @@
 $(document).ready(function(){
     $('.slider').slick({
         slidesToShow: 3,
+        responsive: [
+            {
+                breakpoint:715,
+                settings:{
+                    slidesToShow: 1,   
+                }
+            }
+        ]
   
+    });
+
+    $('.menu-btn').on('click', function(e){
+        e.preventDefault;
+        $(this).toggleClass('menu-btn_active');
+        $('.menu-nav').toggleClass('menu-nav_active');
     });
         
     });
