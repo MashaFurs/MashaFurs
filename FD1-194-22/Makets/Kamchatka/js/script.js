c8013adde5e5e1f4e649925f6819f3b1512f0f3b
MashaFurs <Mashafurs3371403@yandex.ru>
2022-09-26 23:03:30 +0300

    hw

--- FD1-194-22/Makets/Kamchatka/js/script.js
+++ FD1-194-22/Makets/Kamchatka/js/script.js
@@ -17,5 +17,28 @@ $(document).ready(function(){
         $(this).toggleClass('menu-btn_active');
         $('.menu-nav').toggleClass('menu-nav_active');
     });
+
+
+    const cards = document.querySelectorAll('.card');
+
+for (let i = 0; i < cards.length; i++) {
+    const card = cards[i];
+    card.addEventListener('mousemove', startRotate);
+    card.addEventListener('mouseout', stopRotate);
+    
+}
+
+
+function startRotate(event) {
+    const cardItem = this.querySelector('.photo-items');
+    const halfHeight = cardItem.offsetHeight / 2;
+    const halfWidth = cardItem.offsetWidth / 2;
+    cardItem.style.transform = 'rotateX(' + - (event.offsetY - halfHeight) / 5 + 'deg) rotateY(' + (event.offsetX - halfWidth) / 5 + 'deg)';
+}
+
+function stopRotate(event) {
+    const cardItem = this.querySelector('.photo-items');    
+    cardItem.style.transform = 'rotate(0)';
+}
         
     });
