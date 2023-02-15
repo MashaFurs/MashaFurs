561d835ac945d1e5c0e20656ff712db25534e3c6
MashaFurs <Mashafurs3371403@yandex.ru>
2023-02-16 01:29:45 +0300

    hw1

---
+++ FD3-Furs/ishop/Ishop.js
@@ -0,0 +1,37 @@
+﻿var Ishop = React.createClass({
+
+    displayName: 'Ishop',
+  
+    propTypes: {
+      name: React.PropTypes.string.isRequired, 
+      products:React.PropTypes.arrayOf(
+        React.PropTypes.shape({
+          brandTitle: React.PropTypes.string.isRequired,
+          modelTitle: React.PropTypes.string.isRequired,
+          imgUrl: React.PropTypes.string.isRequired,
+          price: React.PropTypes.number.isRequired,
+          key: React.PropTypes.number.isRequired,
+          storage: React.PropTypes.number.isRequired,
+        })
+      )  
+    },
+  
+    render: function() {
+  
+        var productsCode= this.props.products.map( p =>
+            React.DOM.div({key:p.key,className:'card'},
+              React.DOM.img({className:'imgUrl', src: p.imgUrl}),
+              React.DOM.p({className:'brandTitle'},`Марка: ${p.brandTitle}`),
+              React.DOM.p({className:'modelTitle'},`Модель: ${p.modelTitle}`),
+              React.DOM.p({className:'price'},`Цена: ${p.price} $`),
+              React.DOM.p({className:'storage'},`Осталось на складе: ${p.storage} шт.`),
+            )
+          );
+
+      return React.DOM.div( {className:'Ishop'}, 
+        React.DOM.h1( {className:'name'}, this.props.name ),
+        React.DOM.div( {className:'products'}, productsCode ),
+      );
+    },
+  
+  });
