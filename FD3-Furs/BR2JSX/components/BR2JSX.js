ea6eee0420d069d64b3a572f9eb9bc8ef3b7365d
MashaFurs <Mashafurs3371403@yandex.ru>
2023-03-15 20:27:43 +0300

    hw

---
+++ FD3-Furs/BR2JSX/components/BR2JSX.js
@@ -0,0 +1,21 @@
+﻿import React, { Fragment } from 'react';
+
+import './BR2JSX.css';
+
+class BR2JSX extends React.Component {
+
+  render() {
+
+    let words=this.props.text.split(/<br\s?\/?>/);
+    let result=[];
+    for( let i=0; i<words.length; i++) {
+      if (i>0)  {result.push (<br/>);}
+      result.push(words[i]);
+    }
+    return <Fragment>{result}</Fragment>;
+    
+  }
+
+}
+
+export default BR2JSX;
