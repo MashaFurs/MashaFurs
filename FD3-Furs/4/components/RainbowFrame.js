45488de97487303a9b01b68f2fba2fe094292c88
MashaFurs <Mashafurs3371403@yandex.ru>
2023-03-15 15:02:18 +0300

    hw

---
+++ FD3-Furs/4/components/RainbowFrame.js
@@ -0,0 +1,19 @@
+﻿import React from 'react';
+
+import './RainbowFrame.css';
+
+class RainbowFrame extends React.Component {
+
+  render() {
+
+    let code=this.props.children;
+    for( let color of this.props.colors) {
+      code=<div className={color} style={ { border:`solid 10px ${color}`}}>{code}</div>
+    }
+    return code;
+    
+  }
+
+}
+
+export default RainbowFrame;
