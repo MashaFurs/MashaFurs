4cea73f55dbb9e23a746bdaf2f8966625785c4cb
MashaFurs <Mashafurs3371403@yandex.ru>
2023-03-18 21:04:27 +0300

    plus package and data

--- FD3-Furs/RainbowFrameHoc/components/DoubleButton.js
+++ FD3-Furs/RainbowFrameHoc/components/DoubleButton.js
@@ -5,14 +5,15 @@ import './DoubleButton.css';
 class DoubleButton extends React.Component {
 
     btnClick = (eo) => {
-        if(eo.currentTarget.value===this.props.caption1) { this.props.cbPressed(1)}
+      console.log(eo.currentTarget.dataset.id);
+        if(eo.currentTarget.dataset.id==="one") { this.props.cbPressed(1)}
         else {this.props.cbPressed(2)}
       };
 
   render() {
     let text=this.props.children;
     return <div className='first'>
-                <input type="button" value={this.props.caption1} onClick={this.btnClick}/> <span>{text}</span> <input type="button" value={this.props.caption2} onClick={this.btnClick}/>
+                <input type="button" data-id="one" value={this.props.caption1} onClick={this.btnClick}/> <span>{text}</span> <input type="button" data-id="two" value={this.props.caption2} onClick={this.btnClick}/>
           </div>
     
   }
