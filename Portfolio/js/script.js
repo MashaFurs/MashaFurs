4bf3a5e5067c6f1dd0e6d4397931e82d0941cf53
MashaFurs <Mashafurs3371403@yandex.ru>
2023-05-23 11:09:34 +0300

    add5

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,5 +1,20 @@
 
 function submitForm() {
-    document.getElementById('form').submit();
-    document.getElementById('form').reset();
+    
+    let form = document.getElementById('form');
+
+    //Отправка формы через Formspare
+     let xhr = new XMLHttpRequest();
+     xhr.open(form.method, form.action);
+     xhr.setRequestHeader("Accept", "application/json");
+     xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            form.submit();
+            form.reset();
+        } else {
+            console.error('Произошла ошибка');
+        }
+     }
+     xhr.send( new FormData(form));
 }
