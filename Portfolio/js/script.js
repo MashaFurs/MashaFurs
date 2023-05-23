f64200d85619fdd8becf3530ecb73fbbfb5fc8a7
MashaFurs <Mashafurs3371403@yandex.ru>
2023-05-23 14:48:12 +0300

    777

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -2,25 +2,8 @@
 function submitForm() {
     
     let form = document.getElementById('form');
-
     let inputs = document.querySelectorAll('._req');
-
-    let nameInput = document.getElementById('input_name');
-    let emailInput = document.getElementById('input_email');
-    let textInput = document.getElementById('input_text');
-    let textArea = document.getElementById('textarea');
-
     let isValid = true;
-
-    // if(!/\S+/.test(nameInput.value) ||
-    //    !/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(emailInput.value) ||
-    //    !/\S+/.test(textInput.value) ||
-    //    !/\S+/.test(textArea.value)) {
-    //     alert("Заполните все поля формы");
-    //     return;
-    //    }
-
-
 
     for( let i=0; i<inputs.length; i++) {
         let input= inputs[i];
@@ -30,7 +13,6 @@ function submitForm() {
             input.type === "textarea" && !/\S+/.test(input.value)) {
                 input.classList.add('_error');
                 isValid = false;
-
             } else {
                 input.classList.remove('_error');
             }
@@ -40,62 +22,6 @@ function submitForm() {
             alert("Заполните поля");
             return;
            }
-
-
-
-    //    if(!/\S+/.test(nameInput.value)) {
-    //     nameInput.classList.add('_error');
-    //     isValid = false;
-    //    } else {
-    //     nameInput.classList.remove('_error');
-    //    }
-
-    //    if(!/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(emailInput.value)) {
-    //     nameInput.classList.add('_error');
-    //     isValid = false;
-    //    } else {
-    //     nameInput.classList.remove('_error');
-    //    }
-
-    //    if(!/\S+/.test(textInput.value)) {
-    //     nameInput.classList.add('_error');
-    //     isValid = false;
-    //    } else {
-    //     nameInput.classList.remove('_error');
-    //    }
-
-    //    if(!/\S+/.test(textArea.value)) {
-    //     nameInput.classList.add('_error');
-    //     isValid = false;
-    //    } else {
-    //     nameInput.classList.remove('_error');
-    //    }
-
-    //    if (!isValid) {
-    //     alert("Заполните поля");
-    //     return;
-    //    }
-
-
-
-    // function validate() {
-
-    //     let formReq= document.querySelectorAll('._req');
-
-    //     for( let i=0; i<formReq.length; i++) {
-    //         const input= formReq[i];
-    //         input.classList.remove('_error');
-    
-    //         if (input.value === "") {
-    //             input.classList.add('_error');
-    //         }
-    //     }
-    //     return;
-    // }
-
-    // validate();
-    
-
 
 
     //Отправка формы через Formspare
