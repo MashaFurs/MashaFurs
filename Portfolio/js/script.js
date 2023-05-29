a7cb55e503352405331638f39d7da37d4845632b
MashaFurs <Mashafurs3371403@yandex.ru>
2023-05-30 00:50:42 +0300

    add animation

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -1,4 +1,140 @@
 
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+if (ScrollTrigger.isTouch !== 1) {
+
+    gsap.fromTo('.content', { opacity: 1 }, {
+		opacity: 0,
+		scrollTrigger: {
+			trigger: '.content',
+			start: 'center',
+			end: '600',
+			scrub: true
+		}
+	})
+
+    gsap.fromTo('.education', { opacity: 1 }, {
+		opacity: 0,
+		scrollTrigger: {
+			trigger: '.education',
+			start: 'center',
+			end: '740',
+			scrub: true
+		}
+	})
+
+
+    const tl = gsap.timeline()
+
+    tl.fromTo ( 
+        '.hero_name',
+        {
+            x: -150,
+            opacity: 0,
+        },
+        {
+            x: 0,
+            opacity: 1,
+            duration: 1,
+        }).fromTo(
+            '.hero_describe',
+        {
+            y: 50,
+            opacity: 0,
+        },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 0.7,
+        },
+        0.6
+        ).fromTo(
+            '.btnCV',
+        {
+            y: 20,
+            opacity: 0,
+        },
+        {
+            y: 0,
+            opacity: 1,
+            duration: 1,
+        },
+        1.1
+        ).fromTo(
+            '.star',
+        {
+            scale: 0,
+            opacity: 0,
+        },
+        {
+            scale: 1,
+            opacity: 1,
+            duration: 1,
+            stagger: 0.2,
+        },
+        1
+        )
+
+    //Hero scroll animation
+
+
+    gsap.to('.content', {
+        scrollTrigger: {
+            trigger:'.main',
+            start: 'top top',
+            scrub: true,
+        },
+        y:200,
+    })
+
+    gsap.to('.content-text', {
+        scrollTrigger: {
+            trigger:'.main',
+            start: 'top top',
+            scrub: true,
+        },
+        y:100,
+    })
+
+    //Анимация блока skills
+
+    gsap.from('.variant', {
+        scrollTrigger: {
+            trigger: '.skills one',
+            start: 'top top',
+            
+        },
+        scale: 0,
+        transformOrigin: 'top center',
+        ease: 'none',
+        stagger:0.6,
+    })
+
+     //Анимация education
+
+     gsap.to( '.education-foto', {
+        scrollTrigger: {
+            trigger: '.education-foto',
+            start: 'top top',
+            scrub: true,
+        },
+        y: 150,
+     })
+
+     gsap.to( '.education-blocks', {
+        scrollTrigger: {
+            trigger: '.education-blocks',
+            start: 'top top',
+            scrub: true,
+        },
+        y: -200,
+     })
+
+
+    
+}
+
+
 function submitForm() {
     
     let form = document.getElementById('form');
