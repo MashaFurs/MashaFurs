e0ed5d4f067fbd84f5352a99e8f61b188154e89a
MashaFurs <Mashafurs3371403@yandex.ru>
2023-06-02 17:42:51 +0300

    add adaptive and burger

--- Portfolio/js/script.js
+++ Portfolio/js/script.js
@@ -187,3 +187,34 @@ function changeMapLanguage() {
     mapIframe.contentWindow.postMessage('{"event":"command","func":"setOptions","args":[{"language":"en"}]}', '*');
 }
 window.addEventListener('load', changeMapLanguage);
+
+
+const menu = document.querySelector('.menu_body');
+const menuBtn = document.querySelector('.menu_icon');
+const socialBurger = document.querySelector('.social_burger');
+const borderBurger = document.querySelector('.points');
+
+const body = document.body;
+
+if (menu && menuBtn) {
+
+    menuBtn.addEventListener('click', () => {
+        menu.classList.toggle('active');
+        menuBtn.classList.toggle('active');
+        socialBurger.classList.toggle('active');
+        borderBurger.classList.toggle('active');
+
+        body.classList.toggle('lock');
+    })
+
+    menu.querySelectorAll('.menu_link').forEach( link => {
+        link.addEventListener('click', () => {
+            menu.classList.remove('active');
+            menuBtn.classList.remove('active');
+            socialBurger.classList.remove('active');
+            borderBurger.classList.remove('active');
+
+            body.classList.remove('lock');
+        })
+    })
+}
