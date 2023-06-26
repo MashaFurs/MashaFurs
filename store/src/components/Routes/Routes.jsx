aedecf3c46722d634d5e62d999d7aee3d5357c30
MashaFurs <Mashafurs3371403@yandex.ru>
2023-06-26 23:11:22 +0300

    create category and add pagination

--- store/src/components/Routes/Routes.jsx
+++ store/src/components/Routes/Routes.jsx
@@ -4,8 +4,10 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../Home/Home";
 import SingleProduct from "../Products/SingleProduct";
 import Profile from "../Profile/Profile";
+import SingleCategory from "../Categories/SingleCategory";
 
 import { ROUTES } from '../../utils/routes';
+
 
 
 const AppRoutes = () => (
@@ -13,6 +15,7 @@ const AppRoutes = () => (
             <Route index element={<Home />} />
             <Route path={ROUTES.PRODUCT} element={ <SingleProduct/> }/>
             <Route path={ROUTES.PROFILE} element={ <Profile/> }/>
+            <Route path={ROUTES.CATEGORY} element={ <SingleCategory/> }/>
         </Routes>
 );
 
