fbe75550a151fd5e321c1839e8141808c69788a4
MashaFurs <Mashafurs3371403@yandex.ru>
2023-07-26 00:55:00 +0300

    add infinite load

--- IT-Academy/RickAndMorty/src/components/CharacterList.jsx
+++ IT-Academy/RickAndMorty/src/components/CharacterList.jsx
@@ -6,23 +6,50 @@ import CharacterCard from "./CharacterCard";
 const CharacterList = ()=>{
 
     const[characters, setCharacters]= useState([]);
-
+    const[currentPage, setCurrentPage]= useState(1);
+    const[fetching, setFetching]= useState(true);
+    
 
     const fetchCharacters= async () => {
         try {
-            const response = await axios.get('https://rickandmortyapi.com/api/character');
-            setCharacters(response.data.results);
+            const response = await axios.get(`https://rickandmortyapi.com/api/character/?page=${currentPage}`);
+            setCharacters([...characters, ...response.data.results ]);
+            setCurrentPage( prevState => prevState +1);
         } catch (error) {
             console.error('Error fetching characters:', error)
+        } finally {
+            setFetching(false);
         }
-    }
+    };
 
 
     useEffect( ()=>{
 
-        fetchCharacters();
+        if(fetching) {
+            console.log('fetching');
+            fetchCharacters();
+        }
+
+    },[fetching]);
+
+
+    useEffect( ()=>{
+
+        document.addEventListener('scroll', scrollHandler)
+
+        return function () {
+            document.removeEventListener('scroll', scrollHandler)
+        };
 
     },[]);
+
+    const scrollHandler =(e)=>{
+
+        if(e.target.documentElement.scrollHeight - (e.target.documentElement.scrollTop + window.innerHeight) < 100) {
+            setFetching(true);
+        }
+    };
+
 
 
     return ( <div className="character_list">
